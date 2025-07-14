dc316f32bfe636de90674e5776ff5ebc2f10d4b6
Moukthika N <moukinellutla3@gmail.com>
2025-07-14 11:33:02 -0400

    created homepage + intro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import profilePic from './profile.jpeg'; // Place your image in the src folder
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="homepage-container">
+      <div className="left-column">
+        <img src={profilePic} alt="Your Name" className="profile-pic" />
+        <h1 className="name">Moukthika Nellutla</h1>
+        <p className="school">University of Illinois Urbana-Champaign</p>
+        <p className="major">Computer Science + Bioengineering</p>
+      </div>
+      <div className="right-column">
+        <h2>About Me</h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I'm a passionate undergraduate student exploring the intersection of AI and neuroscience.
+          I love building intelligent systems, working on brain-computer interfaces, and using 
+          technology to solve impactful problems.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>
+          Outside of academics, I enjoy chocolatey drinks, coding side projects, and taking long walks 
+          around campus.
+        </p>
+      </div>
     </div>
   );
 }
